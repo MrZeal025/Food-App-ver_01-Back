de4ca0258ad447495eb7478ee5f913c6dcadabcc
MrZeal025 <ryannograles.025.gtis@gmail.com>
2021-02-12 13:42:39 +0800

    fixed admin uploads

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,54 +1,5 @@
-const multer = require("multer");
 const { cloudinary } = require('../config/cloudinary');
 const { User, Admin } = require('../models');
-
-const storage1 = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, process.env.MULTER_STORAGE_DESTINATION_PROFILE_PICTURE);
-    },
-    filename: (req, file, callback) => {
-        const extension = file.originalname.split('.').pop()
-        const processFileName = "xs.prcs." + file.originalname.split(' ').join('.').split('.').join('_') + "." + extension
-        callback(null, processFileName);
-    },
-});
-
-const storage2 = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, process.env.MULTER_STORAGE_DESTINATION_COVER_PHOTO);
-    },
-    filename: (req, file, callback) => {
-        const extension = file.originalname.split('.').pop()
-        const processFileName = "xs.prcs." + file.originalname.split(' ').join('.').split('.').join('_') + "." + extension
-        callback(null, processFileName);
-    },
-});
-
-const storage3 = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, process.env.MULTER_STORAGE_DESTINATION_RECIPE_PHOTO);
-    },
-    filename: (req, file, callback) => {
-        const extension = file.originalname.split('.').pop()
-        const processFileName = "xs.prcs." + file.originalname.split(' ').join('.').split('.').join('_') + "." + extension
-        callback(null, processFileName);
-    },
-});
-
-
-// filter image
-const fileFilter = (req, file, callback) => {
-    if(file.mimetype === 'image/jpeg' || file.mimetype === "image/png") {
-        callback(null, true);
-    }
-    else {
-        callback("Wrong file type has been uploaded...", false);
-    }
-}
-
-var profileUpload = multer({ storage: storage1, limits: { fileSize: 1024 * 1024 * 20 }, fileFilter: fileFilter}).array('profilePicture');
-var coverPhotoUpload = multer({ storage: storage2, limits: { fileSize: 1024 * 1024 * 20}, fileFilter: fileFilter }).array('coverPhoto');
-var multipleUpload = multer({ storage: storage3, limits: { fileSize: 1024 * 1025 * 20 }, fileFilter: fileFilter}).array('recipeImages');
 
 exports.uploadProfile = async (req, res) => {
     try {
@@ -70,31 +21,21 @@ exports.uploadProfile = async (req, res) => {
 
 exports.uploadAdminProfile = async (req, res) => {
     
-    profileUpload(req, res, async function (err) {
-
-        // check if there is a errow on the multer process
-        if (err instanceof multer.MulterError) {
-            throw err
-        } else if (err) {
-            return res.status(400).json({ success: false, data: { messsage: err }})
-        }
-        // check if there is a file
-        if(req.files.length === 0) return res.status(400).json({success: false, data: { messsage: "Profile picture is empty." }});
-        
-        // respond to the user call
-        try {
-            const admin = await Admin.findById(req.params.id);
-            if(!admin) return res.status(400).json({ success: false, data: { message: "ADmin not found"} })
-            
-            admin.profilePicture = req.files[0].filename 
-            await admin.save();
-            
-            return res.status(200).json({ success: true, data: { message: "Profile picture updated successfully.", admin: admin.profilePicture }});
-        } 
-        catch (error) {
-            return res.status(400).json({ success: false, data: { messsage: error.response }})
-        }
-    })
+    try {
+        const fileStr = req.body.data 
+        const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset: "dev_setups"
+        })
+        const admin = await Admin.findById(req.params.id);
+        if(!admin) return res.status(400).json({ success: false, data: { message: "User not found"} })    
+        admin.profilePicture = uploadedResponse.url
+        await admin.save();
+        return res.status(200).json({ success: true, data: { message: "Profile picture updated successfully.", imageResponse: uploadedResponse }});
+    } 
+    catch (error) {
+        console.log(error.response)
+        return res.status(400).json({ success: false, data: { messsage: error.response }})
+    }
 }
 
 exports.uploadCoverPhoto = async (req, res) => {
@@ -117,31 +58,21 @@ exports.uploadCoverPhoto = async (req, res) => {
 
 exports.uploadAdminCoverPhoto = async (req, res) => {
     
-    coverPhotoUpload(req, res, async function (err) {
-
-        // check if there is a errow on the multer process
-        if (err instanceof multer.MulterError) {
-            throw err
-        } else if (err) {
-            return res.status(400).json({ success: false, data: { messsage: err }})
-        }
-        // check if there is a file
-        if(req.files.length === 0) return res.status(400).json({success: false, data: { messsage: "Cover photo is empty." }});
-        
-        // respond to the user call
-        try {
-            const admin = await Admin.findById(req.params.id);
-            if(!admin) return res.status(400).json({ success: false, data: { message: "Admin not found"} })
-            
-            admin.backGroundPicture = req.files[0].filename 
-            await admin.save();
-            
-            return res.status(200).json({ success: true, data: { message: "Cover photo updated successfully.", admin: admin.backGroundPicture }});
-        } 
-        catch (error) {
-            return res.status(400).json({ success: false, data: { messsage: error.response }})
-        }
-    })
+    try {
+        const fileStr = req.body.data 
+        const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset: "dev_setups"
+        })
+        const admin = await Admin.findById(req.params.id);
+        if(!admin) return res.status(400).json({ success: false, data: { message: "User not found"} })    
+        admin.backGroundPicture = uploadedResponse.url
+        await admin.save();
+        return res.status(200).json({ success: true, data: { message: "Cover picture updated successfully.", imageResponse: uploadedResponse }});
+    } 
+    catch (error) {
+        console.log(error.response)
+        return res.status(400).json({ success: false, data: { messsage: error.response }})
+    }
 }
 
 exports.createRecipeImage = async (req, res) => {
